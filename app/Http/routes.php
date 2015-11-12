56bdcd7b88825bdb4299a34f62d79c8cf52d0b85
Loc Dinh <hostinger.dao@gmail.com>
2015-11-12 10:43:38 +0700

    merge routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -75,46 +75,9 @@ Route::group(['prefix' => 'admin','middleware' => 'authrole_ad'], function () {
 	Route::post('transcript', 'Admin\TranscriptController@store');
 });
 
-Route::get('admin/adduser', [
-	'middleware' => 'authrole',
-	'uses' => 'Admin\AdduserController@index',
-	]);
-Route::get ('admin/manage-user/admin', 'Admin\UserManageController@get_ad');
-Route::post('admin/manage-user/admin', 'Admin\UserManageController@store_ad');
-Route::get ('admin/manage-user/admin/delete/{id}', 'Admin\UserManageController@delete_ad');
-Route::get ('admin/manage-user/admin/edit/{id}', 'Admin\UserManageController@get_edit_form');
-Route::post ('admin/manage-user/admin/edit/{id}', 'Admin\UserManageController@edit_ad');
-
-Route::get ('admin/manage-user/teacher', 'Admin\UserManageController@get_te');
-Route::post('admin/manage-user/teacher', 'Admin\UserManageController@store_te');
-Route::get ('admin/manage-user/teacher/delete/{id}', 'Admin\UserManageController@delete_te');
-
-Route::get ('admin/manage-user/student', 'Admin\UserManageController@get_stu');
-Route::post('admin/manage-user/student', 'Admin\UserManageController@store_stu');
-
-Route::get ('admin/manage-user/parent', 'Admin\UserManageController@get_pa');
-Route::post('admin/manage-user/parent', 'Admin\UserManageController@store_pa');
-
-Route::get ('admin/manage-user/userlist', 'Admin\UserManageController@get_userlist');
-
-//Manage class
-
-Route::get('admin/classinfo', 'Classes\ClassController@view');
-
-Route::get('admin/form', 'Classes\ClassController@form');
-
-Route::post('admin/save', 'Classes\ClassController@save');
-
-Route::post('admin/update', 'Classes\ClassController@update');
-
-Route::get('admin/delete/{id}', 'Classes\ClassController@delete');
-
-Route::get('admin/edit/{id}', 'Classes\ClassController@edit');
-
-
 //Manage student of class
 
-Route::get('studentclassinfo', 'StudentInClass\StudentInClassController@view');
+Route::get('class/studentclassinfo', 'StudentInClass\StudentInClassController@view');
 
 Route::post('filterstudent', 'StudentInClass\StudentInClassController@filterstudent');
 
@@ -123,23 +86,4 @@ Route::post('getclass', 'StudentInClass\StudentInClassController@getclass');
 Route::post('addStudent', 'StudentInClass\StudentInClassController@addStudent');
 
 Route::post('removeStudent', 'StudentInClass\StudentInClassController@removeStudent');
-
-
-
-
-Route::get('admin/adduser', [
-	'middleware' => 'authrole',
-	'uses' => 'Admin\AdduserController@index',
-	]);
-
-Route::post('admin/adduser', 'Admin\AdduserController@store');
-
-Route::get('admin/adduser', 'Admin\AdduserController@index');
-Route::post('admin/adduser', 'Admin\AdduserController@store');
-
-
-//Manage subject
-Route::get('admin/addsubject', 'Admin\AddsubjectController@index');
-Route::post('admin/addsubject', 'Admin\AddsubjectController@store');
-
 
