2cdb5dc0821a7d4a964a47340616ae5586b8f2e6
Bao <luu.bao.bk.93@gmail.com>
2016-04-22 08:11:36 +0700

    abcd

--- app/Http/Controllers/Api/MobileScheduleController.php
+++ app/Http/Controllers/Api/MobileScheduleController.php
@@ -82,59 +82,78 @@ class MobileScheduleController extends ApiGuardController
             //
         }
         else if($user->role == 1){
-            $tkb['user'] = $user->id;
-        }
-    }
-
-    public function get_view(){
-        $student = Auth::user();
-        $year = substr(date('Y'),2,2);
-        $month = date('m');
-        $year = ($month < 8 ) ? $year - 1 : $year ;
-        $tkb = [];
-        $check = StudentClass::where('student_id','=',$student->id)
-                             ->where('class_id','like', $year."_%")
-                             ->first();
-        if($check == null){
-            $tkb = "no_class";
-            return view('studentpage.schedule',['tkb' => $tkb]);
-        }
-        else{
-            $class = Classes::find($check->class_id);
-            $teacher_list = Phancong::select('teacher_id')->where('class_id','=',$class->id)->get();
-            if(count($teacher_list) == 0){
-                $tkb =  "no_schedule";
-                return view('studentpage.schedule',['tkb' => $tkb]);
+            $teacher = Auth::user();
+            $year = substr(date('Y'),2,2);
+            $month = date('m');
+            $year = ($month < 8 ) ? $year - 1 : $year ;
+            $tkb = [];
+            $check = Phancong::where('teacher_id','=',$teacher->id)
+                                 ->where('class_id','like', $year."_%")
+                                 ->first();
+            if($check == null){
+                $data['status'] = "no_schedule";
+                return Response::json($data);
             }
             else{
                 $check = tkb::all();
                 if(count($check) == 0){
-                    $tkb =  "no_schedule";
-                    return view('studentpage.schedule',['tkb' => $tkb]);
+                    $data['status'] = "no_schedule";
+                    return Response::json($data);
                 }
-                for($i = 0; $i<=49; $i++){
-                    if($i == 0 || $i == 9){
-                        $tiet['subject'] = "Chào Cờ";
-                    }
-                    else if($i == 44 || $i == 49){
-                        $tiet['subject'] = "SHCN";
+                else{
+                    $temp = tkb::where('teacher_id','=',$teacher->id)->first()->toArray();
+                    if($temp == null){
+                        $data['status'] = "no_schedule";
+                        return Response::json($data);
                     }
                     else{
-                        $tiet = tkb::select('subject_name')
-                              ->whereIn('teacher_id',$teacher_list)
-                              ->where('T'.$i,'=',$class->classname)->first();
-                        if($tiet == null){
-                            $tiet['subject'] = "";
-                        }
-                        else{
-                            $tiet['subject'] = $tiet->subject_name;
-                        }
+                        //nothing to do
                     }
-                    array_push($tkb, $tiet);
+                    $tkb_date = Sysvar::find('tkb_date')->value;
+                    for($i=0;$i<=49;$i++){
+                        $tkb['t'.$i] = $temp['T'.$i];
+                    }
+                    $data['tkb'] = $tkb;
+                    $data['status'] = "schedule";
+                    $data['date'] = $tkb_date;
+                    return Response::json($data);
                 }
-                $tkb_date = Sysvar::find('tkb_date')->value;
-                return view('studentpage.schedule',['tkb' => $tkb, 'tkb_date' => $tkb_date, 'class' => $class]);
             }
         }
     }
+
+    public function get_view(){
+        $teacher = Auth::user();
+        $year = substr(date('Y'),2,2);
+        $month = date('m');
+        $year = ($month < 8 ) ? $year - 1 : $year ;
+        $tkb = [];
+        $check = Phancong::where('teacher_id','=',$teacher->id)
+                             ->where('class_id','like', $year."_%")
+                             ->first();
+        if($check == null){
+            $tkb = "no_placement";
+            return view('teacherpage.schedule',['tkb' => $tkb]);
+        }
+        else{
+            $check = tkb::all();
+            if(count($check) == 0){
+                $tkb = "no_schedule";
+                return view('teacherpage.schedule',['tkb' => $tkb]);
+            }
+            else{
+                $tkb = tkb::where('teacher_id','=',$teacher->id)->first()->toArray();
+                if($tkb == null){
+                    $tkb = "no_schedule";
+                    return view('teacherpage.schedule',['tkb' => $tkb]);
+                }
+                else{
+                }
+                $tkb_date = Sysvar::find('tkb_date')->value;
+                return view('teacherpage.schedule',['tkb' => $tkb, 'tkb_date' => $tkb_date]);
+            }
+        }
+    }
+
+    
 }
